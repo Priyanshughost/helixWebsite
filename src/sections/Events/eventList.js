9487c20b16f929c966a25ac85bdeb55ee0ff9b13
Rohit Chandra <rohitchandra044@gmail.com>
2026-06-26 15:12:14 +0530

    Update eventList.js

--- src/sections/Events/eventList.js
+++ src/sections/Events/eventList.js
@@ -1,7 +1,7 @@
 import hackHorizonWinner from "../../assets/hack-horizon-winner-2025.jpg";
 import ClubOrientation from "../../assets/ClubOrientation1.jpg";
    export const eventList = [
-    { title: "Club Orientation", date: "Jan 27, 2025", img: "https://images.unsplash.com/photo-1528605248644-14dd04022da1?q=80&w=500&auto=format&fit=crop" },
+    { title: "Club Orientation", date: "Jan 27, 2025", img: ClubOrientation,},
     { title: "SDE Workshop", date: "Feb 19, 2025", img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=500&auto=format&fit=crop" },
     { title: "Club Induction", date: "Aug 29, 2025", img: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=500&auto=format&fit=crop" },
     { title: "DevSpark", date: "Sep 4, 2025", img: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=500&auto=format&fit=crop" },
@@ -12,7 +12,7 @@ import ClubOrientation from "../../assets/ClubOrientation1.jpg";
     { title: "Martinovation", date: "Nov 3-7, 2025", img: "https://images.unsplash.com/photo-1515187029135-18ee286d815b?q=80&w=500&auto=format&fit=crop" },
     { title: "RanchiHacks", date: "Jan 17-18, 2026", img: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=500&auto=format&fit=crop" },
     { title: "NPTEL Workshop", date: "Jan 22, 2026", img: "https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=500&auto=format&fit=crop" },
-    { title: "Hack Horizon", date: "Apr 11-12, 2025", img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=500&auto=format&fit=crop"},
+    { title: "Hack Horizon", date: "Apr 11-12, 2025", img: hackHorizonWinner,},
     { title: "Eklavya Techfest", date: "Apr 23-24, 2025", img: "https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=500&auto=format&fit=crop" },
     { title: "Hackatron 25", date: "May 11-12, 2025", img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=500&auto=format&fit=crop" },
 ]
