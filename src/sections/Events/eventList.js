cc94f95aee6ba585d74dfff87cd30d027f107e4b
Rohit Chandra <rohitchandra044@gmail.com>
2026-06-26 14:51:16 +0530

    Update eventList.js

--- src/sections/Events/eventList.js
+++ src/sections/Events/eventList.js
@@ -1,17 +1,6 @@
 import hackHorizonWinner from "../../assets/hack-horizon-winner-2025.jpg";
 import ClubOrientation from "../../assets/ClubOrientation1.jpg";
 import SdeWorkshop from "../../assets/SDEWorkshop.jpg";
-import ClubInduction from "../../assets/ClubInduction.jpg";
-import DevSpark from "../../assets/DevSpark.jpg";
-import SIH-Internal-Hackathon from "../../assets/SIH-Internal-Hackathon.jpg";
-import AI-Workshop from "../../assets/AI-Workshop.jpg";
-import HackQuibt from "../../assets/HackQuibt.jpg";
-import DevFest from "../../assets/SDevFest.jpg";
-import Martinovation from "../../assets/Martinovation.jpg";
-import RanchiHacks from "../../assets/RanchiHacks.jpg";
-import NPTEL from "../../assets/NPTEL.jpg";
-import Eklavya-Techfest from "../../assets/Eklavya-Techfest.jpg";
-import Hackatron25 from "../../assets/Hackatron25.jpg";
 export const eventList = [
     { title: "Club Orientation", date: "Jan 27, 2025", img: ClubOrientation,},
     { title: "SDE Workshop", date: "Feb 19, 2025", img: SdeWorkshop,},
