9933590bcfca41ed85a2d5d692cb7e6ba20925f0
Abhijeet <ghoshabhijit1295@gmail.com>
2026-06-28 10:11:55 +0530

    image upt

--- src/sections/Events/eventList.js
+++ src/sections/Events/eventList.js
@@ -1,18 +1,31 @@
-import hackHorizonWinner from "../../assets/hack-horizon-winner-2025.jpg";
-import ClubOrientation from "../../assets/ClubOrientation1.jpg";
-   export const eventList = [
-    { title: "Club Orientation", date: "Jan 27, 2025", img: ClubOrientation,},
-    { title: "SDE Workshop", date: "Feb 19, 2025", img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=500&auto=format&fit=crop" },
-    { title: "Club Induction", date: "Aug 29, 2025", img: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=500&auto=format&fit=crop" },
-    { title: "DevSpark", date: "Sep 4, 2025", img: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=500&auto=format&fit=crop" },
-    { title: "SIH Internal Hackathon", date: "Sep 22, 2025", img: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?q=80&w=500&auto=format&fit=crop" },
-    { title: "AI Workshop", date: "Sep 24, 2025", img: "https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=500&auto=format&fit=crop" },
-    { title: "Hack Qubit", date: "Oct 7-8, 2025", img: "https://images.unsplash.com/photo-1504384764586-bb4cdc1707b0?q=80&w=500&auto=format&fit=crop" },
-    { title: "DevFest Ranchi", date: "Oct 11, 2025", img: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=500&auto=format&fit=crop" },
-    { title: "Martinovation", date: "Nov 3-7, 2025", img: "https://images.unsplash.com/photo-1515187029135-18ee286d815b?q=80&w=500&auto=format&fit=crop" },
-    { title: "RanchiHacks", date: "Jan 17-18, 2026", img: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=500&auto=format&fit=crop" },
-    { title: "NPTEL Workshop", date: "Jan 22, 2026", img: "https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=500&auto=format&fit=crop" },
-    { title: "Hack Horizon", date: "Apr 11-12, 2025", img: hackHorizonWinner,},
-    { title: "Eklavya Techfest", date: "Apr 23-24, 2025", img: "https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=500&auto=format&fit=crop" },
-    { title: "Hackatron 25", date: "May 11-12, 2025", img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=500&auto=format&fit=crop" },
-]
+import ClubOrientation from "../../assets/ClubOrientation.jpg";
+import SDEWorkshop from "../../assets/SDEWorkshop.jpg";
+import ClubInduction from "../../assets/ClubInduction.jpg";
+import DevSpark from "../../assets/DevSpark.jpg";
+import SIHInternalHackathon from "../../assets/SIH-Internal-Hackathon.jpg";
+import AIWorkshop from "../../assets/AI-Workshop.jpg";
+import HackQubit from "../../assets/HackQuibt.jpg";
+import DevFestRanchi from "../../assets/devFest25.jpg";
+import Martinovation from "../../assets/Martinovation.jpg";
+import RanchiHacks from "../../assets/RanchiHacks.jpg";
+import NPTELWorkshop from "../../assets/NPTEL.jpg";
+import HackHorizonWinner from "../../assets/hack-horizon-winner-2025.jpg";
+import EklavyaTechfest from "../../assets/Eklavya-Techfest.jpg";
+import Hackatron25 from "../../assets/Hackatron25.jpg";
+
+export const eventList = [
+    { title: "Club Orientation", date: "Jan 27, 2025", img: ClubOrientation },
+    { title: "SDE Workshop", date: "Feb 19, 2025", img: SDEWorkshop },
+    { title: "Club Induction", date: "Aug 29, 2025", img: ClubInduction },
+    { title: "DevSpark", date: "Sep 4, 2025", img: DevSpark },
+    { title: "SIH Internal Hackathon", date: "Sep 22, 2025", img: SIHInternalHackathon },
+    { title: "AI Workshop", date: "Sep 24, 2025", img: AIWorkshop },
+    { title: "Hack Qubit", date: "Oct 7-8, 2025", img: HackQubit },
+    { title: "DevFest Ranchi", date: "Oct 11, 2025", img: DevFestRanchi },
+    { title: "Martinovation", date: "Nov 3-7, 2025", img: Martinovation },
+    { title: "RanchiHacks", date: "Jan 17-18, 2026", img: RanchiHacks },
+    { title: "NPTEL Workshop", date: "Jan 22, 2026", img: NPTELWorkshop },
+    { title: "Hack Horizon", date: "Apr 11-12, 2025", img: HackHorizonWinner },
+    { title: "Eklavya Techfest", date: "Apr 23-24, 2025", img: EklavyaTechfest },
+    { title: "Hackatron 25", date: "May 11-12, 2025", img: Hackatron25 },
+];
