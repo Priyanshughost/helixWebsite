d4e5cb73c4ea09f1ba86ec74504a4ae5d982850f
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2026-06-28 11:57:48 +0530

    Refactor Home page by removing unused components
    Removed ImageGallery and Fader2 components from the Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,8 @@ import collection from '../sections/ImageGallery/collection';
 const Reel = lazy(() => import('../sections/Reel'));
 const Tagline = lazy(() => import('../sections/Tagline'));
 const About = lazy(() => import('../sections/About'));
-const ImageGallery = lazy(() => import('../sections/ImageGallery/ImageGallery'));
 const EventSection = lazy(() => import('../sections/Events/EventSection'));
 const Fader = lazy(() => import('../sections/Fader'));
-const Fader2 = lazy(() => import('../sections/Fader2'));
 const TeamsV2 = lazy(() => import('../sections/TeamsV2'));
 const HallOfFame = lazy(() => import('../sections/HallOfFame'));
 const Testimonials = lazy(() => import('../sections/Testimonials'));
@@ -25,12 +23,10 @@ function Home() {
       <Suspense fallback={<div className="w-full h-screen bg-black"></div>}>
         <Reel />
         <Tagline />
-        <About />
 
         <Fader />
-        <ImageGallery items={collection} />
-        <Fader2 />
-
+        
+        <About />
         <EventSection />
         <HallOfFame />
         <Partnerships />
